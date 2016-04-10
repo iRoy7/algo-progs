f81347325ed077628034986a18481836b2211bc4
iRoy7 <myoungwoon@gmail.com>
2016-04-10 21:25:35 +0900

    Create hanoi_recursion.cpp

---
+++ hanoi_recursion.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+inline void __hanoi_recursion(int num, int from, int to, int spare)
+{
+	if (num == 1) {
+		printf("%d -> %d\n", from, to);
+		return;
+	}
+	else {
+		__hanoi_recursion(num - 1, from, spare, to);
+		__hanoi_recursion(1, from, to, spare);
+		__hanoi_recursion(num - 1, spare, to, from);
+	}
+	return;
+}
+int main()
+{
+	int N;
+	setbuf(stdout, NULL);
+	scanf("%d", &N);
+	if (N > 1)
+		printf("%d\n", (1 << N) - 1);
+	else
+		printf("%d\n", N);
+	__hanoi_recursion(N, 1, 3, 2);
+	return 0;
+}
