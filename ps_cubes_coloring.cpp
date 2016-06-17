3ef252b0d174d36fade4ed08e21527a7df10be59
iRoy7 <myoungwoon@gmail.com>
2016-06-17 23:42:32 +0900

    Update ps_cubes_coloring.cpp

--- ps_cubes_coloring.cpp
+++ ps_cubes_coloring.cpp
@@ -1,45 +1,59 @@
 #include <stdio.h>
-#define CMAX 1000000
-long long int y_aspt[CMAX + 1];
-long long int x_aspt[CMAX + 1];
-long long int z_aspt[CMAX + 1];
-long long int yx_aspt[CMAX + 1];
-long long int yxz_aspt[CMAX + 1];
-int T, N;
-int x, y, z, a, b, c;
-inline int abs(int a) { return (a > 0) ? a : (-a); }
-inline void memory_clear() {
-	for (int i = 0; i < CMAX + 1; i++)
+#define SZ_C 1000001
+
+int y_aspt[SZ_C];
+int x_aspt[SZ_C];
+int z_aspt[SZ_C];
+long long yx_aspt[SZ_C];
+long long yxz_aspt[SZ_C];
+
+// cube's color i = |x-A| + |y-B| + |z-C| = i(mod)N
+int N; // N->color
+int x, y, z, A, B, C;
+
+inline int
+ABS(int a) { return (a > 0) ? a : (-a); }
+
+static void
+clear_buf()
+{
+	int i;
+	for (i = 0; i < SZ_C; i++)
 		y_aspt[i] = x_aspt[i] = z_aspt[i] = yx_aspt[i] = yxz_aspt[i] = 0;
 }
+
 int main()
 {
-	int i, j;
 	setbuf(stdout, NULL);
+
+	int T, tc, i, j;
 	scanf("%d", &T);
-	while (T--) {
-		// inputs:
-		scanf("%d%d%d %d%d%d %d", &x, &y, &z, &a, &b, &c, &N);
-		memory_clear();
-		// solve: 1. y, x, z aspects
+
+	for (tc = 1; tc <= T; tc++)
+	{
+		scanf("%d%d%d %d%d%d %d", &x, &y, &z, &A, &B, &C, &N);
+
 		for (i = 0; i < y; i++)
-			y_aspt[abs(i - b) % N]++;
+			y_aspt[ABS(i - B) % N]++;
 		for (i = 0; i < x; i++)
-			x_aspt[abs(i - a) % N]++;
+			x_aspt[ABS(i - A) % N]++;
 		for (i = 0; i < z; i++)
-			z_aspt[abs(i - c) % N]++;
-		// solve: 2. y*x aspect
+			z_aspt[ABS(i - C) % N]++;
+
 		for (i = 0; i < N; i++)
 			for (j = 0; j < N; j++)
 				yx_aspt[(i + j) % N] += y_aspt[i] * x_aspt[j];
-		// solve: 3. y*x*z aspect
+
 		for (i = 0; i < N; i++)
 			for (j = 0; j < N; j++)
 				yxz_aspt[(i + j) % N] += yx_aspt[i] * z_aspt[j];
-		// outputs:
+
 		for (i = 0; i < N; i++)
 			printf("%lld ", yxz_aspt[i]);
 		printf("\n");
+
+		clear_buf();
 	}
+
 	return 0;
 }
