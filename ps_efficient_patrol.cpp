a6ce2e13ce01f9faadc05f8a1ac356d4459deece
iRoy7 <myoungwoon@gmail.com>
2016-06-17 22:48:26 +0900

    Update ps_efficient_patrol.cpp

--- ps_efficient_patrol.cpp
+++ ps_efficient_patrol.cpp
@@ -1,63 +1,103 @@
 #include <stdio.h>
-#define MAXIS 1000
-int n, evn;
-int row[MAXIS + 10], col[MAXIS + 10];
-int dt[MAXIS + 10][MAXIS + 10];
-inline void memory_clear()
+#define SZ_N 1001
+// row->y, col->x
+int row[SZ_N];
+int col[SZ_N];
+
+int N, evnts;
+// Dynamic Table
+int DT[SZ_N][SZ_N];
+
+inline int
+min(int a, int b) { return (a > b) ? b : a; }
+inline int 
+ABS(int a) { return (a > 0) ? a : (-a); }
+
+static void
+clear_buf()
 {
-	for (int i = 0; i < MAXIS + 10; i++)
-		for (int j = 0; j < MAXIS + 10; j++)
-			dt[i][j] = -1;
+	int i;
+	for (i = 0; i < SZ_N; i++)
+		row[i] = col[i] = 0;
+	for (i = 0; i < SZ_N*SZ_N; i++)
+		*((int *)DT + i) = -1;
 }
-inline int abs(int a) { return (a > 0) ? a : (-a); }
-inline int min(int a, int b) { return (a > b) ? b : a; }
-inline int dist(int a, int b)
+
+static int
+get_Dist(int a, int b)
 {
-	return abs(row[a] - row[b]) + abs(col[a] - col[b]);
+	return ABS(row[a] - row[b]) + ABS(col[a] - col[b]);
 }
-inline int minDist(int here, int another)
+
+static int
+min_Dist(int here, int another)
 {
-	if (here == evn) return 0;
-	int &ret = dt[here][another];
+	int &ret = DT[here][another];
 	if (ret != -1) return ret;
+
+	// base cases
+	if (here == evnts) return 0;
+
 	int next = here + 1;
-	return ret = min(minDist(next, another) + dist(here, next),
-		minDist(next, here) + dist(another, next));
+	return ret = min(min_Dist(next, another) + get_Dist(here, next),
+		min_Dist(next, here) + get_Dist(another, next));
 }
-inline void construct(int here, int another, int car)
+
+static void
+construct(int here, int another, int car)
 {
-	if (here == evn) return;
+	if (here == evnts) return;
 	int next = here + 1;
-	if (minDist(next, another) + dist(here, next) <
-		minDist(next, here) + dist(another, next)) {
+	if (min_Dist(next, another) + get_Dist(here, next)
+		< min_Dist(next, here) + get_Dist(another, next))
+	{
 		printf("%d\n", car);
 		construct(next, another, car);
 	}
-	else {
+	else
+	{
 		printf("%d\n", 3 - car);
 		construct(next, here, 3 - car);
 	}
 }
+
 int main()
 {
-	scanf("%d %d", &n, &evn), evn++;
-	row[0] = col[0] = 1, row[1] = col[1] = n;
-	// inputs:
-	for (int i = 2; i <= evn; i++) scanf("%d %d", &row[i], &col[i]);
-	memory_clear();
-	// solve: 1. calculate min distances
-	int a = minDist(2, 1) + dist(0, 2);
-	int b = minDist(2, 0) + dist(1, 2);
-	//printf("%d %d\n", a, b);
-	// outputs:
-	printf("%d\n", min(a, b));
-	if (a < b) {
-		puts("1");
-		construct(2, 1, 1);
+	freopen("events_info_for_patroling.txt", "r", stdin);
+	setbuf(stdout, NULL);
+
+	int T, tc, i, a, b;
+	scanf("%d", &T);
+	for (tc = 1; tc <= T; tc++)
+	{
+		scanf("%d%d", &N, &evnts);
+
+		evnts++;
+		row[0] = col[0] = 1; // 첫번째 사건
+		row[1] = col[1] = N; // 두번째 사건
+		// 실제 입력되는 사건 3번째라고 두고,
+		for (i = 2; i <= evnts; i++)
+			scanf("%d%d", &row[i], &col[i]);
+
+		a = b = 0;
+		a = min_Dist(2, 1) + get_Dist(0, 2);
+		b = min_Dist(2, 0) + get_Dist(1, 2);
+
+		printf("%d\n", min(a, b));
+
+		if (a < b)
+		{
+			puts("1");
+			construct(2, 1, 1);
+		}
+		else
+		{
+			puts("2");
+			construct(2, 0, 2);
+		}
+
+		clear_buf();
 	}
-	else {
-		puts("2");
-		construct(2, 0, 2);
-	}
+
 	return 0;
 }
