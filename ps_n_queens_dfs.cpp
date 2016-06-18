a9734c53b5b9c27b75fbbaf7af3185fca6db0dc4
iRoy7 <myoungwoon@gmail.com>
2016-06-18 11:31:11 +0900

    Update ps_n_queens_dfs.cpp

--- ps_n_queens_dfs.cpp
+++ ps_n_queens_dfs.cpp
@@ -1,23 +1,25 @@
-Enter file contents here#include <stdio.h>
+
+#include <stdio.h>
 #define MAXIS 11
+
 int n, cnt;
 int col[MAXIS];
 int icr[2 * MAXIS];
 int dcr[2 * MAXIS];
 
 static void
-putQueensDFS(int r)
+putQueensDFS(int row)
 {
-	if (r > n) {
-	    cnt++;
-		return;
+	if (row > n) {
+	    cnt++; // No. of way which can put all Queues in rows
+		return; // back tracking
 	}
 
 	for (int i = 1; i <= n; i++) {
-		if (!col[i] && !icr[r + i] && !dcr[(r - i) + n]) {
-			col[i] = icr[r + i] = dcr[(r - i) + n] = 1;
-			putQueensDFS(r + 1);
-			col[i] = icr[r + i] = dcr[(r - i) + n] = 0;
+		if (!col[i] && !icr[row + i] && !dcr[(row - i) + n]) {
+			col[i] = icr[row + i] = dcr[(row - i) + n] = 1; // mark
+			putQueensDFS(row + 1);
+			col[i] = icr[row + i] = dcr[(row - i) + n] = 0; // un-mark
 		}
 	}
 }
