eb9dcab02ff25f4162cdd03cd428a007947a1027
iRoy7 <myoungwoon@gmail.com>
2016-06-20 23:37:01 +0900

    Update bs_recursion_hanoi_towers.cpp

--- bs_recursion_hanoi_towers.cpp
+++ bs_recursion_hanoi_towers.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
-inline void __hanoi_recursion(int num, int from, int to, int spare)
+
+static void 
+__hanoi_recursion(int num, int from, int to, int spare)
 {
 	if (num == 1) {
 		printf("%d -> %d\n", from, to);
@@ -12,15 +14,19 @@ inline void __hanoi_recursion(int num, int from, int to, int spare)
 	}
 	return;
 }
+
 int main()
 {
-	int N;
 	setbuf(stdout, NULL);
+	
+	int N;
 	scanf("%d", &N);
 	if (N > 1)
 		printf("%d\n", (1 << N) - 1);
 	else
 		printf("%d\n", N);
+		
 	__hanoi_recursion(N, 1, 3, 2);
+	
 	return 0;
 }
