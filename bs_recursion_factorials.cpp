f7c76e169a6ad9d6139ce636e73b469df0cd0bf1
iRoy7 <myoungwoon@gmail.com>
2016-06-20 23:35:57 +0900

    Update bs_recursion_factorials.cpp

--- bs_recursion_factorials.cpp
+++ bs_recursion_factorials.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int a[11];
-inline int __print_n(int n)
+
+static int
+__print_n(int n)
 {
 	if (n == 0) return 0;
 	else {
@@ -8,7 +10,9 @@ inline int __print_n(int n)
 		return __print_n(n - 1);
 	}
 }
-inline int __recursion_fact(int n)
+
+static int
+__recursion_fact(int n)
 {
 	if (n == 0 || n == 1) return 1;
 	return n * __recursion_fact(n - 1);
@@ -16,9 +20,11 @@ inline int __recursion_fact(int n)
 
 int main()
 {
-	__print_n(10);
-	int i;
 	setbuf(stdout, NULL);
+	
+	__print_n(10);
+
+	int i;
 	// inputs:
 	for (i = 0; i < 11; i++)
 		scanf("%d", &a[i]);
