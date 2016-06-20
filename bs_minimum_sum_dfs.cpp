6705328fab827df7e7267155808fff709e1d4b8e
iRoy7 <myoungwoon@gmail.com>
2016-06-20 23:29:32 +0900

    Update bs_minimum_sum_dfs.cpp

--- bs_minimum_sum_dfs.cpp
+++ bs_minimum_sum_dfs.cpp
@@ -1,40 +1,47 @@
 #include <stdio.h>
-#define MAXIS 11
+#define SZ_N 11
 
 int N;
-int TI[MAXIS][MAXIS];
-int COL[MAXIS];
+int TI[SZ_N][SZ_N];
+int COL[SZ_N];
 int minSum = 2147483647;
 
 static void
-minSumDFS(int r, int sum)
+minSumDFS(int row, int sum)
 {
-	if (r == N) {
-		if (minSum > sum) minSum = sum;
+	if (row == N) {
+		if (sum < minSum) minSum = sum;
 		return;
 	}
+	
 	for (int i = 0; i < N; i++) {
 #if 0 // except the selected column
 		if (!COL[i]) {
 			COL[i] = 1; // mark;
-			minSumDFS(r + 1, sum + TI[r][i]);
+			minSumDFS(row + 1, sum + TI[row][i]);
 			COL[i] = 0; // unmark;
 		}
 #else // all cases
-		minSumDFS(r + 1, sum + TI[r][i]);
+		minSumDFS(row + 1, sum + TI[row][i]);
 #endif
 	}
+	
 	return;
 }
+
 int main()
 {
 	setbuf(stdout, NULL);
 	scanf("%d", &N);
+	
 	int i, j;
 	for (i = 0; i < N; i++)
 		for (j = 0; j < N; j++)
 			scanf("%d", &TI[i][j]);
+	
 	minSumDFS(0, 0);
+	
 	printf("minSum = %d\n", minSum);
+	
 	return 0;
 }
