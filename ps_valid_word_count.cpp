fd80d37a36d82a7498a0ca77ed13f988180fb1cb
iRoy7 <myoungwoon@gmail.com>
2016-06-21 23:29:22 +0900

    Update ps_valid_word_count.cpp

--- ps_valid_word_count.cpp
+++ ps_valid_word_count.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define MAXIS 1000000
-int T;
+
 char qKey[MAXIS + 10];
 char dWord[MAXIS + 10];
 int wNum;
@@ -8,23 +8,28 @@ int wNum;
 int a[8][3] = { 
 	{97, 99}, {100, 102}, {103, 105}, {106, 108}, {109, 111},
 	{112, 115}, {116, 118}, {119, 122} };
-inline void memory_clear()
+
+static void
+memory_clear()
 {
 	for (int i = 0; i < 1000010; i++)
 		qKey[i] = 0, dWord[i] = 0;
 }
 int main()
 {
-	int i, j, tc;
 	setbuf(stdout, NULL);
+	
+	int T, tc, i, j;
 	scanf("%d", &T);
-	for (tc = 1; tc <= T; tc++) {		
-		memory_clear();
+
+	for (tc = 1; tc <= T; tc++) 
+	{		
 		// inputs:		
 		scanf("%s %d", qKey, &wNum);
 		// solve: 1. create total min/max
 		int min = 0, max = 0;
-		for (i = 0; qKey[i]; i++) {
+		for (i = 0; qKey[i]; i++) 
+		{
 			int x = qKey[i] - '0';
 			min += a[x - 2][0];
 			max += a[x - 2][1];
@@ -32,7 +37,8 @@ int main()
 		//printf("%d %d\n", min, max);
 		// solve: 2. check boundary
 		int valid_w_cnt = 0;
-		for (i = 0; i < wNum; i++) {
+		for (i = 0; i < wNum; i++) 
+		{
 			scanf("%s", dWord);
 			//printf("%s\n", dWord);
 			int sum = 0;
@@ -43,6 +49,9 @@ int main()
 		}		
 		// outouts:
 		printf("#%d %d\n", tc, valid_w_cnt);
+		// clear buffer
+		memory_clear();
 	}
+	
 	return 0;
 }
