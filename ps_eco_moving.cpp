e805e913ed0051ce2399a5d3bd419215fccf7e9c
iRoy7 <myoungwoon@gmail.com>
2016-06-21 23:55:46 +0900

    Update ps_eco_moving.cpp

--- ps_eco_moving.cpp
+++ ps_eco_moving.cpp
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #define MAX_MAP 10010
 #define MAX_N 1010
+
 struct Crossroad {
 	int y, x;
 };
@@ -17,31 +18,40 @@ struct Crossroad cross[MAX_N];
 
 int W, H, N;
 
-inline int abs(int a) { return (a > 0)? a : (-a); }
+inline int 
+ABS(int a) { return (a > 0)? a : (-a); }
+
+
+static void
+clear_memory() 
+{
+	for (int i = 0; i < MAX_N; i++)
+		cross[i].y = 0, cross[i].x = 0;
+}
 
 static int
-diagonal_paths(int y1, int y2, int x1, int x2) {
+diagonal_paths(int y1, int y2, int x1, int x2) 
+{
 	// move (y1, x1)->(y2, x2)
 	int path_cnt = 0;
-	while ((y1 < y2 && x1 < x2) || (y1 > y2 && x1 > x2)) {
+	while ((y1 < y2 && x1 < x2) || (y1 > y2 && x1 > x2)) 
+	{
 		path_cnt++;
 		if (y1 < y2 && x1 < x2) y1++, x1++;
 		else y2++, x2++;
 	}
+	
 	return path_cnt;
 }
 
-static void
-clear_memory() {
-	for (int i = 0; i < MAX_N; i++)
-		cross[i].y = 0, cross[i].x = 0;
-}
 int main()
 {
 	freopen("moving_info.txt", "r", stdin);
 	setbuf(stdout, NULL);
+
 	int T, tc, i, j;
 	scanf("%d", &T);
+
 	for (tc = 1; tc <= T; tc++) {
 		int min_fare = 0;
 		// inputs:
@@ -58,9 +68,9 @@ int main()
 		}
 		// ouputs:
 		printf("#%d %d\n", tc, min_fare);
-		
 		// clear memory
 		clear_memory();
 	}
+
 	return 0;
 }
