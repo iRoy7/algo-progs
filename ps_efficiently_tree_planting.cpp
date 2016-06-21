6890eb9f39c24d94f9842f956656c49e0ac5d658
iRoy7 <myoungwoon@gmail.com>
2016-06-21 23:52:46 +0900

    Update ps_efficiently_tree_planting.cpp

--- ps_efficiently_tree_planting.cpp
+++ ps_efficiently_tree_planting.cpp
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #define MAXIS 1000
+
 int n, m, cnt;
 int a[MAXIS*MAXIS+1000];
-inline void memory_clear() {
+
+static void 
+memory_clear() {
   cnt = 0;
   for (int i = 0; i < (MAXIS*MAXIS)+1000; i++)
     a[i] = 0;
 }
-int main() {
-  int i, j;
+
+int main() 
+{
   setbuf(stdout, NULL);
+  
   scanf("%d %d", &n, &m);
-  memory_clear();
+
+  int i, j;
   for (i = 1; i <= n*m; i+=4)
     a[i]++, a[i+1]++;
   for (i = 1; i <= n*m; i++)
     if (a[i]) cnt++;
+    
   printf("%d\n", cnt);
+  
+  memory_clear();
 }
