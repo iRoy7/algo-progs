c48e2363f68cac241901548a4568d0409d89f0e7
iRoy7 <myoungwoon@gmail.com>
2016-06-22 00:12:08 +0900

    Update cr_q1_01_is_unique.cpp

--- cr_q1_01_is_unique.cpp
+++ cr_q1_01_is_unique.cpp
@@ -1,36 +1,51 @@
 // Assumes only letters a through z.
 #include <stdio.h>
 #define MAXIS 512
+
 char str[MAXIS + 1];
-int n, len;
-inline void clear_mem();
-inline bool is_uniq_chars(char *str, int len)
+
+static void 
+clear_buf()
 {
-	if (len > 26) return false; // only 26 characters
+	for (int i = 0; i < MAXIS + 1; i++)
+		str[i] = 0;
+}
+
+static bool 
+is_uniq_chars(char *str, int len)
+{
+  // base cases: only 26 characters
+	if (len > 26) return false; 
+	
 	int checker = 0;
-	for (int i = 0; i < len; i++) {
+	for (int i = 0; i < len; i++) 
+	{
 		int val = str[i] - 'a';
 		// check의 val bit가 켜져있는지?
-		if ((checker & (1ull << val)) > 0) return false;
+		if ((checker & (1ull << val)) > 0)
+		  return false;
 		// 원소추가: bit turn on
 		checker |= (1ull << val);
 	}
+	
 	return true;
 }
+
 int main()
 {
 	freopen("words.txt", "r", stdin);
 	setbuf(stdout, NULL);
+	
+	int n;
 	scanf("%d", &n);
-	while (n--) {
+	
+	while (n--) 
+	{
 		scanf("%s", str);
 		for (len = 0; str[len]; len++);
 		printf("%d\n", is_uniq_chars(str, len));
-		clear_mem();
+		clear_buf();
 	}
-}
-inline void clear_mem()
-{
-	for (int i = 0; i < MAXIS + 1; i++)
-		str[i] = 0;
+	
+	return 0;
 }
