bda6840b0c93f510f99ef69a2103135cf0c3b68d
iRoy7 <myoungwoon@gmail.com>
2016-06-21 23:22:02 +0900

    Update topc_friends_score.cpp

--- topc_friends_score.cpp
+++ topc_friends_score.cpp
@@ -36,22 +36,25 @@ output:
 #include <stdio.h>
 #define MAX_F 51
 
-inline int max(int a, int b) { return (a > b) ? a : b; }
+inline int 
+max(int a, int b) { return (a > b) ? a : b; }
 
 int main()
 {
 	freopen("friends_info.txt", "r", stdin);
 	setbuf(stdout, NULL); 
 
-	int T, N, i, j, k;
 	static char FRNS[MAX_F][MAX_F];
+	int T, N, i, j, k;
 	scanf("%d", &T);
 
 	while (T--) {
+		// input
 		scanf("%d", &N);
 		for (i = 0; i < N; i++)
 			scanf("%s", &FRNS[i]);
 
+		// solve
 		int ans = -2147483648;
 		for (i = 0; i < N; i++)
 		{
@@ -82,8 +85,10 @@ int main()
 			ans = max(ans, (d_fnd_cnt + ind_fnd_cnt));
 		}
 
+		// output
 		printf("%d\n", ans);
 
+		// clear buffer
 		for (i = 0; i < N; i++)
 			for (j = 0; j < N; j++)
 				FRNS[i][j] = 0;
